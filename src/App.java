c68473a8a48fcf09ed7dcda5cbf4c6e55f049067
BachilyPC <adrian.munoz01@epn.edu.ec>
2026-01-11 20:17:26 -0500

    UserTypeDAO

--- src/App.java
+++ src/App.java
@@ -1,8 +1,5 @@
-import DataAccessComponent.AdministratorDAO;
-import DataAccessComponent.DTO.AdministratorDTO;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
     }
 }
