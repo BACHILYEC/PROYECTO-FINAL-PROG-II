15c9de5b601272fede46bec4800a39c2ab122c2d
BachilyPC <adrian.munoz01@epn.edu.ec>
2026-01-11 23:29:34 -0500

    Heidytest

--- src/App.java
+++ src/App.java
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-public class App {
-    public static void main(String[] args) throws Exception {
-=======
 import DataAccessComponent.AdministratorDAO;
 import DataAccessComponent.PlayerDAO;
 import DataAccessComponent.DTO.AdministratorDTO;
@@ -15,11 +10,8 @@ public class App {
         // PlayerDTO newPlayer = new PlayerDTO(1,1, "4x4", 100);
         // player.create(newPlayer);
 
-        for(PlayerDTO p : player.readAllstatus(true)){
+        for (PlayerDTO p : player.readAllstatus(true)) {
             System.out.println(p.toString());
         }
-
-
->>>>>>> 409865ba2e11b96bd26a0f35097c14754615be79
     }
 }
