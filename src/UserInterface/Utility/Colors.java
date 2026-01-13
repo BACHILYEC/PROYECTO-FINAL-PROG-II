1e2234536149dfbcf755e73b9dbed9e70d3b6029
BachiMini <adrian.munoz01@epn.edu.ec>
2026-01-13 12:41:57 -0500

    Colores,PruebaAdmin

---
+++ src/UserInterface/Utility/Colors.java
@@ -0,0 +1,10 @@
+package UserInterface.Utility;
+
+import java.awt.Color;
+
+public class Colors {
+    public static Color Color() {
+        Color color = new Color(100, 149, 237);
+        return color;
+    }
+}
---
+++ src/UserInterface/Utility/ReusableMethods.java
@@ -0,0 +1,49 @@
+package UserInterface.Utility;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+
+import DataAccessComponent.DAOs.UserAdminDAO;
+import DataAccessComponent.DTOs.UserAdminDTO;
+
+public class ReusableMethods {
+    public static JScrollPane createTable(String[] columnNames, JPanel panel, Boolean status) {
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+        JTable data = new JTable(model) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        TableColumnModel columnModel = data.getColumnModel();
+        columnModel.getColumn(0).setPreferredWidth(80);
+        columnModel.getColumn(1).setPreferredWidth(150);
+        JScrollPane scrollPane = new JScrollPane(data);
+        model.setRowCount(0);
+        UserAdminDAO dao = new UserAdminDAO();
+        try {
+            for (UserAdminDTO dto : dao.readAllstatus(status)) {
+                String[] row = { dto.getUserName(), dto.getLastLogin(), dto.getIdAdministrator().toString() };
+                model.addRow(row);
+            }
+        } catch (
+
+        Exception ex) {
+            JOptionPane.showMessageDialog(panel, "Error al obtener datos: " + ex.getMessage());
+        }
+        return scrollPane;
+    }
+
+    public static void setContentPane(JPanel panel, JFrame frame) {
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.revalidate();
+        frame.repaint();
+    }
+}
