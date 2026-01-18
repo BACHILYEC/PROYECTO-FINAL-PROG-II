0085ddfa80476092f614a025c1a7fb243f9e2c12
KarliBoo <karli_cruz29@hotmail.com>
2026-01-18 18:17:34 -0500

    Embelleciendo

--- src/App.java
+++ src/App.java
@@ -1,12 +1,9 @@
 import DataAccessComponent.DAOs.AnswerDAO;
 import DataAccessComponent.DTOs.AnswerDTO;
+import UserInterface.MainMenu.MainMenu;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        AnswerDAO dao = new AnswerDAO();
-        for (AnswerDTO answer : dao.readOptions(48, true)) {
-            System.out.println(answer.toString());
-        }
+        MainMenu.gameMenu();
     }
 }
